4c1e657d08ebcc11af74db51f7aeb4a6246319db
RafaelSouza41 <77064662+RafaelSouza41@users.noreply.github.com>
2026-03-09 16:55:44 -0300

    Update comment in config.py to reflect libraries

--- config/config.py
+++ config/config.py
@@ -1,4 +1,4 @@
-# Databricks notebook source
+# Bibliotecas
 import os
 from datetime import datetime
 from pyspark.sql.functions import col, current_date, when, regexp_replace,split, right,substring, to_date
